e903c616a47842fea9577599a52ea2c10a33cdc2
Dustin Coates <dustin@dcoates.com>
2015-12-29 15:10:14 +0100

    Add new loaders to webpack

--- infinite-scroll/webpack.config.js
+++ infinite-scroll/webpack.config.js
@@ -1,19 +1,33 @@
 var path = require('path')
-  , HtmlWebpackPlugin = require('html-webpack-plugin');
+  , HtmlWebpackPlugin = require('html-webpack-plugin')
+  , ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 module.exports = {
   entry: {
-    app: ["./src/js/app.js"]
+    app: ['./src/js/app.js']
   },
   output: {
-    path: path.resolve(__dirname, "build"),
-    filename: "js/app.js"
+    path: path.resolve(__dirname, 'build'),
+    filename: 'js/app.js'
   },
   plugins: [
     new HtmlWebpackPlugin({
       template: 'src/index.html',
       inject: 'body'
-    })
+    }),
+    new ExtractTextPlugin('css/[name].css')
   ],
+  module: {
+    loaders:[
+      {
+        test: /\.(jpe?g|png|gif|svg)$/,
+        loader: 'url-loader?limit=8192'
+      },
+      {
+        test:  /\.css$/,
+        loader: ExtractTextPlugin.extract('style-loader', 'css-loader')
+      }
+    ]
+  },
   watch: true
 }
