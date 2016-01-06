634dfbbee312704bc539d34d948277b8f83ceb69
Dustin Coates <dustin@dcoates.com>
2016-01-06 12:40:27 -0500

    Match color variants to style enforced by eslint

--- variants_colors/js/app.js
+++ variants_colors/js/app.js
@@ -1,7 +1,6 @@
-$(document).ready(function() {
+/* global $, Hogan, algoliasearch, algoliasearchHelper */
 
-
-
+$(document).ready(function () {
   // INITIALIZATION
   // ==============
 
@@ -18,23 +17,21 @@ $(document).ready(function() {
   var algoliaHelper = algoliasearchHelper(algolia, INDEX_NAME, PARAMS);
 
   // DOM BINDING
-  $searchInput = $('#search-input');
-  $searchInputIcon = $('#search-input-icon');
-  $main = $('main');
-  $hits = $('#hits');
+  var $searchInput = $('#search-input');
+  var $searchInputIcon = $('#search-input-icon');
+  var $main = $('main');
+  var $hits = $('#hits');
 
   // Hogan templates binding
   var hitTemplate = Hogan.compile($('#hit-template').text());
   var noResultsTemplate = Hogan.compile($('#no-results-template').text());
-
-
 
   // SEARCH BINDING
   // ==============
 
   // Input binding
   $searchInput
-  .on('keyup', function() {
+  .on('keyup', function () {
     $('#query-message').empty();
     var query = $(this).val();
     toggleIconEmptyInput(query);
@@ -43,12 +40,12 @@ $(document).ready(function() {
   .focus();
 
   // Search errors
-  algoliaHelper.on('error', function(error) {
+  algoliaHelper.on('error', function (error) {
     console.log(error);
   });
 
   // Search results
-  algoliaHelper.on('result', function(content, state) {
+  algoliaHelper.on('result', function (content) {
     renderHits(content);
     handleNoResults(content);
   });
@@ -56,16 +53,12 @@ $(document).ready(function() {
   // Initial search
   algoliaHelper.search();
 
-
-
   // RENDER SEARCH COMPONENTS
   // ========================
 
   function renderHits(content) {
     $hits.html(hitTemplate.render(content));
   }
-
-
 
   // NO RESULTS
   // ==========
@@ -79,35 +72,31 @@ $(document).ready(function() {
     $hits.html(noResultsTemplate.render({query: content.query}));
   }
 
-
-
   // EVENTS BINDING
   // ==============
 
-  $searchInputIcon.on('click', function(e) {
+  $searchInputIcon.on('click', function (e) {
     e.preventDefault();
     $searchInput.val('').keyup().focus();
   });
-  $(document).on('click', '.clear-all', function(e) {
+  $(document).on('click', '.clear-all', function (e) {
     e.preventDefault();
     $searchInput.val('').focus();
     algoliaHelper.setQuery('').search();
   });
-  $(document).on('click', '.set-distinct', function(e) {
+  $(document).on('click', '.set-distinct', function (e) {
     e.preventDefault();
     $('#query-message').empty();
     setDistinct($(this));
     algoliaHelper.search();
   });
-  $(document).on('click', '.set-query', function(e) {
+  $(document).on('click', '.set-query', function (e) {
     e.preventDefault();
     $('#query-message').html($(this).data('message'));
-    setDistinct($(".set-distinct[data-value='"+$(this).data('distinct')+"']"));
+    setDistinct($(".set-distinct[data-value='" + $(this).data('distinct') + "']"));
     setQuery($(this).data('query'));
     algoliaHelper.search();
   });
-
-
 
   // HELPER METHODS
   // ==============
@@ -126,7 +115,4 @@ $(document).ready(function() {
   function toggleIconEmptyInput(query) {
     $searchInputIcon.toggleClass('empty', query.trim() !== '');
   }
-
-
-
 });
