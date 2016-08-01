ef0460137eaebe99be8f6893cdef8cbf71c5f8b9
kokliKo <nicolas.baissas@gmail.com>
2016-08-01 16:29:00 -0700

    [instant-search-demo] fixing config issues in the push.rb file

--- instant-search/instantsearch.js/push.rb
+++ instant-search/instantsearch.js/push.rb
@@ -30,20 +30,19 @@ Algolia.init :application_id => APPLICATION_ID, :api_key => API_KEY
 index = Algolia::Index.new(INDEX_BASE)
 
 default_settings = {
-  attributesToIndex: %w(brand name categories hierarchicalCategories unordered(description)),
+  attributesToIndex: %w(brand unordered(name) unordered(categories) unordered(description)),
   customRanking: ['desc(popularity)'],
-  attributesForFaceting: %w(brand price_range categories hierarchicalCategories type price),
+  attributesForFaceting: %w(brand categories free_shipping hierarchicalCategories.lvl0 hierarchicalCategories.lvl1 hierarchicalCategories.lvl2 price price_range type rating hierarchicalCategories.lvl3 hierarchicalCategories.lvl4 popularity),
   minWordSizefor1Typo: 3,
-  minWordSizefor2Typos: 7
+  minWordSizefor2Typos: 7,
+  ignorePlurals: true
 }
 
-index_settings = default_settings.merge({
-                                            ignorePlurals: true,
-                                            slaves: %W(#{INDEX_BASE}_price_desc #{INDEX_BASE}_price_asc)
-                                        })
 
-price_desc_settings = default_settings.merge({ ranking: %w(desc(price) typo geo words proximity attribute exact custom) })
-price_asc_settings = default_settings.merge({ ranking: %w(asc(price) typo geo words proximity attribute exact custom) })
+
+index_settings = default_settings.merge({ slaves: %W(#{INDEX_BASE}_price_desc #{INDEX_BASE}_price_asc) })
+price_desc_settings = default_settings.merge({ ranking: %w(desc(price) typo geo words proximity attribute exact custom), typoTolerance: "min" })
+price_asc_settings = default_settings.merge({ ranking: %w(asc(price) typo geo words proximity attribute exact custom), typoTolerance: "min" })
 
 index.set_settings(index_settings)
 Algolia::Index.new("#{INDEX_BASE}_price_desc").set_settings(price_desc_settings)
